9d9290accbac369aa2562ecb1d4a9055df2cb016
Graham Dianaty <gmdianaty@gmail.com>
2016-06-22 20:01:37 -0400

    Completed some promises...
    In the recent Voga Software Video Changelog, I made some promises like a better-looking launch bat/sh file. So here it is, thats all this update carries with it. Sorry :(

--- conquade.bat
+++ conquade.bat
@@ -4,4 +4,6 @@ set DEBUG=conquade:*
 echo Setting URL of view to localhost:8080
 set PORT=8080
 echo Actually running conquade xD
+echo Setting down in the directory you told me to live in...
+cd <WHERE_THIS-APP_LIVES>
 set DEBUG=conquade:* && set PORT=80 && node ./bin/www
--- conquade.sh
+++ conquade.sh
@@ -4,4 +4,6 @@ set DEBUG=conquade:*
 echo Setting URL of view to localhost:8080
 set PORT=8080
 echo Actually running conquade xD
+echo Setting down in the directory you have set to be the app's maindir
+cd <DIRECTORY_WHRERE_I_LIVE>
 set DEBUG=conquade:* && set PORT=80 && node ./bin/www
